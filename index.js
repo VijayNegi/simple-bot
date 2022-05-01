041c8202e01884e97d1ee1b7bf3692a0fc9b85b1
vijay <astro.vijay@gmail.com>
2022-05-01 16:24:56 +0530

    adding live server for keeping alive on replit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
+
 const Discord = require("discord.js")
 require("dotenv").config()
 
