c023db6341ea5bd8550b45e799d2503b349141fa
vijay <astro.vijay@gmail.com>
2022-05-01 16:20:24 +0530

    disable debug logs for now

--- handlers/events.js
+++ handlers/events.js
@@ -39,10 +39,10 @@ function triggerEventHandler(bot, event, ...args){
 function initEvents(bot){
     const {client} = bot
 
-    client.on("debug",(e)=> {
-        triggerEventHandler(bot,"debug",e)
-    })
-    
+    // client.on("debug",(e)=> {
+    //     triggerEventHandler(bot,"debug",e)
+    // })
+
     client.on("ready",()=> {
         triggerEventHandler(bot,"ready")
     })
