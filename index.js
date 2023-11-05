896aa1e8f5475d2e7ab1ed8dfedc432b6e086ff7
Vijay Singh Negi <vinegi@adobe.com>
2023-11-05 23:21:37 +0530

    update for local run

--- index.js
+++ index.js
@@ -33,23 +33,14 @@ client.events = new Discord.Collection();
 client.slashcommands = new Discord.Collection();
 
 client.loadEvents = (bot, reload) => require("./handlers/events")(bot,reload)
-client.loadCommands = (bot,reload) => require("./handlers/commands")(bot,reload)
+//client.loadCommands = (bot,reload) => require("./handlers/commands")(bot,reload)
 client.loadSlashCommands = (bot,reload) => require("./handlers/slashcommands")(bot,reload)
 
 client.loadEvents(bot, false)
-client.loadCommands(bot,false)
+//client.loadCommands(bot,false)
 client.loadSlashCommands(bot,false)
 
 module.export = bot
-
-// client.on("ready",()=>{
-//     console.log(`Logged in as ${client.user.tag}`)
-// })
-// client.on("messageCreate",(message)=>{
-//     if(message.content == "hi"){
-//         message.reply("Hello Worlds!")
-//     }
-// })
 
 connectdb("lcpre")
 client.login(process.env.TOKEN)
